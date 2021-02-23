14cb64d1381cb9b0ff5544f070697d4e2fff2186
Randy <randycarrero9@gmail.com>
2021-02-23 15:53:22 -0500

    Clean up on Code

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,10 @@ class HomePageState extends State<HomePage> {
   List data;
 
   Future<String> getData() async {
-    var response = await http.get(Uri.encodeFull("https://raw.githubusercontent.com/randycarrero/GunplaJsonbase/master/data/model.json"), headers: {
-      "Accept": "application/json"
-    });
+    var response = await http.get(
+        Uri.encodeFull(
+            "https://raw.githubusercontent.com/randycarrero/GunplaJsonbase/master/data/model.json"),
+        headers: {"Accept": "application/json"});
 
     this.setState(() {
       data = json.decode(response.body);
@@ -30,14 +31,10 @@ class HomePageState extends State<HomePage> {
   }
 
   @override
-  void initState() {
-    this.getData();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Gunpla Database"), backgroundColor: Colors.blue),
+      appBar: new AppBar(
+          title: new Text("Gunpla Database"), backgroundColor: Colors.blue),
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
@@ -78,32 +75,51 @@ class HomePageState extends State<HomePage> {
         itemBuilder: (BuildContext context, int index) {
           return new Card(
             elevation: 8.0,
-            margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0, bottom: 4.0),
+            margin:
+                EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0, bottom: 4.0),
             color: Colors.white,
             child: new Container(
-              child: new Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: <Widget>[
-                new Row(mainAxisAlignment: MainAxisAlignment.start, mainAxisSize: MainAxisSize.max, crossAxisAlignment: CrossAxisAlignment.center, children: <Widget>[
-                  new Icon(Icons.accessibility, color: const Color(0xFF666565)),
-                  new Container(
-                      margin: EdgeInsets.only(left: 5.0),
-                      constraints: BoxConstraints(minWidth: 100, maxWidth: 300),
-                      child: Text(
-                        data[index]["title"],
-                        style: new TextStyle(fontSize: 16.0, color: const Color(0xFF000000), fontWeight: FontWeight.w500, fontFamily: "Roboto"),
-                        maxLines: 1,
-                      ))
-                ]),
-                new Container(
-                    alignment: Alignment.topLeft,
-                    margin: EdgeInsets.only(left: 15.0),
-                    constraints: BoxConstraints(minWidth: 100, maxWidth: 300),
-                    child: Text(
-                      data[index]["series"],
-                      style: new TextStyle(fontSize: 14.0, color: const Color(0xFF2d2424), fontWeight: FontWeight.w300, fontFamily: "Roboto"),
-                      maxLines: 4,
-                    ))
-              ]),
-              padding: const EdgeInsets.only(left: 5.0, right: 5.0, top: 15.0, bottom: 15.0),
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          new Icon(Icons.accessibility,
+                              color: const Color(0xFF666565)),
+                          new Container(
+                              margin: EdgeInsets.only(left: 5.0),
+                              constraints:
+                                  BoxConstraints(minWidth: 100, maxWidth: 300),
+                              child: Text(
+                                data[index]["title"],
+                                style: new TextStyle(
+                                    fontSize: 16.0,
+                                    color: const Color(0xFF000000),
+                                    fontWeight: FontWeight.w500,
+                                    fontFamily: ""),
+                                maxLines: 1,
+                              ))
+                        ]),
+                    new Container(
+                        alignment: Alignment.topLeft,
+                        margin: EdgeInsets.only(left: 15.0),
+                        constraints:
+                            BoxConstraints(minWidth: 100, maxWidth: 300),
+                        child: Text(
+                          data[index]["series"],
+                          style: new TextStyle(
+                            fontSize: 14.0,
+                            color: const Color(0xFF2d2424),
+                          ),
+                          maxLines: 4,
+                        ))
+                  ]),
+              padding: const EdgeInsets.only(
+                  left: 5.0, right: 5.0, top: 15.0, bottom: 15.0),
               alignment: Alignment.center,
             ),
           );
