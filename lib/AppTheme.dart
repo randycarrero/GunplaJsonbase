82fdf8630cc5ef7c4fc93badd3114ac2ca39321c
Randy Carrero <randycarrero9@gmail.com>
2021-03-11 05:52:18 -0500

    A-0.3.0 Theme

---
+++ lib/AppTheme.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  //
+  AppTheme._();
+
+  static final ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.teal,
+    appBarTheme: AppBarTheme(
+      color: Colors.teal,
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+    ),
+    cardTheme: CardTheme(
+      color: Colors.teal,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white54,
+    ),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Colors.white,
+      ),
+      subtitle1: TextStyle(
+        color: Colors.white70,
+      ),
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: AppBarTheme(
+      color: Colors.black,
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+    ),
+    cardTheme: CardTheme(
+      color: Colors.black,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white54,
+    ),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Colors.white,
+      ),
+      subtitle1: TextStyle(
+        color: Colors.white70,
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,57 @@
-import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'AppStateNotifier.dart';
 
-void main() {
-  runApp(new MaterialApp(home: new HomePage()));
+class ThemeDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ThemeDemoState();
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() => new HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  List data;
-
-  Future<String> getData() async {
-    var response = await http.get(
-        Uri.encodeFull(
-            "https://raw.githubusercontent.com/randycarrero/gunplajson/main/model.json"),
-        headers: {"Accept": "application/json"});
-
-    this.setState(() {
-      data = json.decode(response.body);
-    });
-
-    print(data[1]["title"]);
-
-    return "Success!";
-  }
-
-  @override
-  // ignore: must_call_super
-  void initState() {
-    this.getData();
-  }
-
+class ThemeDemoState extends State<ThemeDemo> {
+  //
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("Gunpla Database"), backgroundColor: Colors.blue),
-      body: new ListView.builder(
-        itemCount: data == null ? 0 : data.length,
-        itemBuilder: (BuildContext context, int index) {
-          return new Card(
-            elevation: 8.0,
-            margin:
-                EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0, bottom: 4.0),
-            color: Colors.white,
-            child: new Container(
-              child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    new Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          new Icon(Icons.accessibility,
-                              color: const Color(0xFF666565)),
-                          new Container(
-                              margin: EdgeInsets.only(left: 5.0),
-                              constraints:
-                                  BoxConstraints(minWidth: 100, maxWidth: 300),
-                              child: Text(
-                                data[index]["title"],
-                                maxLines: 1,
-                              ))
-                        ]),
-                    new Container(
-                        alignment: Alignment.topLeft,
-                        margin: EdgeInsets.only(left: 15.0),
-                        constraints:
-                            BoxConstraints(minWidth: 100, maxWidth: 300),
-                        child: Text(
-                          data[index]["series"],
-                          maxLines: 4,
-                        ))
-                  ]),
-              padding: const EdgeInsets.only(
-                  left: 5.0, right: 5.0, top: 15.0, bottom: 15.0),
-              alignment: Alignment.center,
-            ),
-          );
-        },
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Flutter Themes'),
+        leading: Icon(Icons.menu),
+        actions: <Widget>[
+          Switch(
+            value: Provider.of<AppStateNotifier>(context).isDarkMode,
+            onChanged: (boolVal) {
+              Provider.of<AppStateNotifier>(context).updateTheme(boolVal);
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, pos) {
+            return Card(
+              elevation: 0,
+              child: ListTile(
+                title: Text(
+                  'Title $pos',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                subtitle: Text(
+                  'Subtitle $pos',
+                  style: Theme.of(context).textTheme.subtitle,
+                ),
+                leading: Icon(
+                  Icons.alarm,
+                  color: Theme.of(context).iconTheme.color,
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Theme.of(context).iconTheme.color,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
